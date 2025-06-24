dda14b20437eb88d7987875ab5d4ae50850e35f3
Harpreet25222 <harpreetsingh25222@gmail.com>
2025-06-24 08:29:23 +0000

    variables and data_types

---
+++ basics/data_types.js
@@ -0,0 +1,25 @@
+// console.log("helo");
+/*
+
+number = 2 to power 53 */
+let a  = 123 
+
+
+/* bigint = this is used for very large amounts */
+ let b = 123456789;
+
+ /*Boolean to gives output in true or false  */
+ let c = false;
+
+ /* very important data type is  object, null , undefined */
+
+ console.log(typeof b);
+
+
+
+
+
+ 
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const a = "happy singh";
+let b = 123;
+var c = "test";
+
+console.table([a, b, c]);
+
+
+/* Const = we can not change the const value after declare at once
+developer develop let to resolve the problem of function scope and scope
+
+please prefer to not to use var in any function  */
