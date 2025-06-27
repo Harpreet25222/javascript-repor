954138f754839d4d436fc7577dfb6d4df7d27150
Harpreet25222 <harpreetsingh25222@gmail.com>
2025-06-27 11:35:58 +0000

    string introduction and basics

---
+++ basics/string_intro.js
@@ -0,0 +1,11 @@
+console.log("tes");
+
+let test = new String('Happy');    /* this is return a string into console as key value pairs */
+
+// console.log(test[0]);                 /*  this will return just H  */
+
+console.log(test.__proto__);            /*  this will return blank object */
+
+
+
+
